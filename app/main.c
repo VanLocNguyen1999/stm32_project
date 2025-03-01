03187fd2f7d1701dadcd51a4966a287ac17bfb67
VanLocNguyen1999 <Locnv@selex.vn>
2025-03-01 16:55:43 +0700

    add stm32f401rct6

--- app/main.c
+++ app/main.c
@@ -9,38 +9,25 @@
 
 uint16_t AD_RES = 0;
 
-int main(void){
+int main(void) {
 
 	board_hardware_init_ex();
 
-	PWM_TIMER->CCR1 = 0.2*PWM_TIMER_TOP_COUNTER;
-	PWM_STARTUP;
-	PWM_ENABLE_CHANNEL;
-	PWM_ENABLE_OUTPUT;
-    // Calibrate The ADC On Power-Up For Better Accuracy
-    HAL_ADCEx_Calibration_Start(&hadc1);
-	HAL_ADC_Start(&hadc1);
-	// Poll ADC1 Perihperal & TimeOut = 1mSec
-	HAL_ADC_PollForConversion(&hadc1, 100);
-	// Read The ADC Conversion Result & Map It To PWM DutyCycle
-	AD_RES = HAL_ADC_GetValue(&hadc1);
-	while(1){
+	while (1) {
 
-		// Start ADC Conversion
-		HAL_ADC_Start(&hadc1);
-		// Poll ADC1 Perihperal & TimeOut = 1mSec
-		HAL_ADC_PollForConversion(&hadc1, 100);
-		// Read The ADC Conversion Result & Map It To PWM DutyCycle
-		AD_RES = HAL_ADC_GetValue(&hadc1);
 		delay_ms(1);
-		if(AD_RES > 2040){
+		if (AD_RES > 2040) {
 			lcd_gotoxy(8, 1);
 			lcd_puts("volt > 2.5");
-		}else {
+		} else {
 			lcd_gotoxy(8, 1);
 			lcd_puts("volt < 2.5");
 		}
 	}
 }
 
+void HAL_IncTick(void){
 
+	uwTick += uwTickFreq;
+
+}
